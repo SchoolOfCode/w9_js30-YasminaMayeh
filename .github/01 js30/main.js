5ee87078e652aa734826ae5ee5443ee44bd8bb81
Yasminamayeh <yasminmayeh@gmail.com>
2021-06-05 02:34:10 +0100

    finished

--- .github/01 js30/main.js
+++ .github/01 js30/main.js
@@ -1,8 +1,8 @@
 const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
 
-
+  
 // plan
 //  step 1 move clock hand (secs) after one sec
 // repeat step 1 for 59 secs
@@ -11,21 +11,22 @@ const hourHand = document.querySelector('.hour-hand');
 // select the second hand in dom = secondsHand
 // create function 
 
-function setDate(){
-    const current = new Date();
+function setDate() {
+    const now = new Date();
      
-    const seconds = current.getSeconds();
-    const secondDegrees = ((seconds/ 60)*360) + 90;
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = current.getMinutes();
+    const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    
 }
 setInterval(setDate, 1000);
 
-  setDate();
+setDate();
