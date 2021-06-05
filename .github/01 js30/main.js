4be40c47d2f1dbeacfc38af455752086aeb06ccf
Yasminamayeh <yasminmayeh@gmail.com>
2021-06-05 02:12:53 +0100

    seconds

--- .github/01 js30/main.js
+++ .github/01 js30/main.js
@@ -1,3 +1,10 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const current = new Date();
+     
+    const seconds = current.getSeconds();
+    const secondDegrees = ((seconds/ 60)*360) + 90;
+}
